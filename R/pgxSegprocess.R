3337d7f8f8aa45e8c6d62a7e9c047b7d4ce6494a
Baudisgroup Admin <zhaohj314@gmail.com>
2022-11-14 16:27:06 +0100

    pgxSegprocess:make group_id flexible

--- R/pgxSegprocess.R
+++ R/pgxSegprocess.R
@@ -4,16 +4,17 @@
 #' supports survival data visualization  
 #'
 #' @param file A string specifying the path and name of the "pgxseg" file where the data is to be read. 
+#' @param group_id A string specifying which id is used for grouping in KM plot or CNV frequency calculation. Default is "group_id".
 #' @param show_KM_plot A logical value determining whether to return the Kaplan-Meier plot based on metadata. Default is FALSE.
 #' @param return_metadata A logical value determining whether to return metadata. Default is FALSE.
 #' @param return_seg A logical value determining whether to return segment data. Default is FALSE.
-#' @param return_frequency A logical value determining whether to return frequency data. The frequency calculation is based on segments in segment data and group id in metadata. Default is FALSE.
+#' @param return_frequency A logical value determining whether to return CNV frequency data. The frequency calculation is based on segments in segment data and specified group id in metadata. Default is FALSE.
 #' @param assembly A string specifying which genome assembly version should be applied to CNV frequency calculation and plotting. Allowed options are "hg19", "hg38". Default is "hg38".
 #' @param ... Other parameters relevant to KM plot. These include `pval`, `pval.coord`, `pval.method`, `conf.int`, `linetype`, and `palette` (see ggsurvplot from survminer)
 #' @return Segments data, CNV frequency object, meta data or KM plots from "pgxseg" files
 #' @export
 
-pgxSegprocess <- function(file,show_KM_plot=F,return_metadata=F,return_seg=F,return_frequency=F,assembly='hg38',...){
+pgxSegprocess <- function(file,group_id = 'group_id', show_KM_plot=F,return_metadata=F,return_seg=F,return_frequency=F,assembly='hg38',...){
   if(!any(show_KM_plot, return_metadata, return_seg, return_frequency)){return()}
   full.data <- readLines(file)
   idx <- grep("#sample=>",full.data)
@@ -27,6 +28,7 @@ pgxSegprocess <- function(file,show_KM_plot=F,return_metadata=F,return_seg=F,ret
   })
   meta <- dplyr::bind_rows(meta)
   rownames(meta) <- seq(dim(meta)[1])
+  if (!group_id %in% colnames(meta)){stop(paste0('group_id: ',group_id,' is not in metadata'))}
   if (show_KM_plot == T){
         # remove samples without survival data
     meta.sel.df <- meta[!is.na(meta$followup_time) & meta$followup_state_id != 'EFO:0030039',]
@@ -34,10 +36,10 @@ pgxSegprocess <- function(file,show_KM_plot=F,return_metadata=F,return_seg=F,ret
       # transform ISOdate interal to days
     meta.sel.df$followup_time <- lubridate::time_length(meta.sel.df$followup_time,unit='day')
       # Map EFO code to numerical representation for plotting
+    meta.sel.df$group_id <- meta.sel.df[,group_id]
     meta.sel.df$followup_state_id[meta.sel.df$followup_state_id == "EFO:0030041"] <- 0 # alive
     meta.sel.df$followup_state_id[meta.sel.df$followup_state_id == "EFO:0030049"] <- 1 # death
     meta.sel.df$followup_state_id <- as.numeric(meta.sel.df$followup_state_id )
-      
     sfit <- survival::survfit(survival::Surv(followup_time, followup_state_id)~group_id , data=meta.sel.df)
     
     splot.op <- list(
@@ -57,12 +59,13 @@ pgxSegprocess <- function(file,show_KM_plot=F,return_metadata=F,return_seg=F,ret
     print(ggp$plot)
     }
    
-  if (return_metadata == T){
+  if (return_metadata == T | return_frequency == T){
     meta$followup_time <- ifelse(length(meta$followup_time) == 0,NA,round(lubridate::time_length(meta$followup_time,unit='day')))
-    meta$followup_state_id[meta$followup_state_id == "EFO:0030041"] <- 'alive'
-    meta$followup_state_id[meta$followup_state_id == "EFO:0030049"] <- 'dead'
-    meta$followup_state_id[meta$followup_state_id == "EFO:0030039"] <- 'unknown'
-    meta <- dplyr::rename(meta,'followup_time(days)' = followup_time, 'followup_state'=followup_state_id)
+    meta <- dplyr::mutate(meta,followup_state_label = NA,.after = followup_state_id)
+    meta$followup_state_label[meta$followup_state_id == "EFO:0030041"] <- 'alive'
+    meta$followup_state_label[meta$followup_state_id == "EFO:0030049"] <- 'dead'
+    meta$followup_state_label[meta$followup_state_id == "EFO:0030039"] <- 'unknown'
+    meta <- dplyr::rename(meta,'followup_time(days)' = followup_time)
   }
   
   if (return_seg == T | return_frequency == T){
@@ -75,8 +78,8 @@ pgxSegprocess <- function(file,show_KM_plot=F,return_metadata=F,return_seg=F,ret
       frequency[['data']] <- list()
       frequency[['meta']] <- c()
       total_sample_count <- 0
-      for (id in unique(meta$group_id)){
-        plot.samples <- meta[,1][meta$group_id %in% id]
+      for (id in unique(meta[,group_id])){
+        plot.samples <- meta[,1][meta[,group_id] %in% id]
         # select samples from samples in pgxseg data
         plot.idx <- rownames(bin.dup.data) %in%  plot.samples
         nsamples <- sum(plot.idx)
@@ -97,8 +100,11 @@ pgxSegprocess <- function(file,show_KM_plot=F,return_metadata=F,return_seg=F,ret
         }
         
         frequency$data[[id]] <- freq.seg
+        group_label <- sub("_id", "_label",group_id)
         frequency$meta <- rbind(frequency$meta, data.frame(group_id = id, 
-                                                           group_label=unique(meta$group_label[meta$group_id == id]),
+                                                           group_label=ifelse(group_label %in% colnames(meta),
+                                                                              unique(meta[,group_label][meta[,group_id] == id]),
+                                                                              NA),
                                                            sample_count=nsamples))
         total_sample_count <-  total_sample_count+nsamples
       }
