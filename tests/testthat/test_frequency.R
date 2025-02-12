33f6c1ab711690102bb52847403de18633c8ad0d
hangjiaz <zhaohj314@gmail.com>
2025-02-12 14:43:54 +0100

    v1.3.3 updates

--- tests/testthat/test_frequency.R
+++ tests/testthat/test_frequency.R
@@ -1,29 +1,17 @@
 context("Retrieve frequency")
 require(httr)
 
-test_that("retrieve frequencies with pgxfreq by filter",{
-#    url_e1 <- "http://www.progenetix.org/services/intervalFrequencies/?output=pgxseg&filters=43"
-    url <- "http://www.progenetix.org/services/intervalFrequencies/?filters=NCIT:C4323,pgx:icdom-85003&output=pgxfreq"
+test_that("retrieve frequencies by filter",{
+    url <- "http://www.progenetix.org/services/intervalFrequencies/?filters=NCIT:C4323,pgx:icdom-85003"
     cat(paste("\n trying:",url,"\n"))
-    r <- GET(url)
-    expect_equal(http_type(r), "text/plain")
-    expect_no_error(result  <- read.table(url, header=T, sep="\t"))
-    expect_equal(dim(result),c(6212,7))
+    result <-  content(GET(url))
+    expect_equal(length(result$response$results),2)
+    expect_equal(length(result$response$results[[1]]$intervalFrequencies),3106)
 })
 
 
-test_that("retrieve frequencies with pgxmatrix by filter",{
-    #url_e1 <- "http://www.progenetix.org/services/intervalFrequencies/?output=pgxmatrix&filters=NCIT:C1234"
-    url <- "http://www.progenetix.org/services/intervalFrequencies/?output=pgxmatrix&filters=NCIT:C4323,pgx:icdom-85003"
-    cat(paste("\n trying:",url,"\n"))
-    r <- GET(url)
-    expect_equal(http_type(r), "text/plain")
-    expect_no_error(result  <- read.table(url, header=T, sep="\t"))
-    expect_equal(dim(result),c(2,6213))
-})
-
-# test_that("retrieve frequencies with pgxmatrix by filter in cellz",{
-#     url <- "https://cancercelllines.org/services/intervalFrequencies/?output=pgxmatrix&filters=NCIT:C5228,pgx:icdom-87203"
+# test_that("retrieve frequencies by filter in cellz",{
+#     url <- "https://cancercelllines.org/services/intervalFrequencies/?filters=NCIT:C5228,pgx:icdom-87203"
 #     cat(paste("\n trying:",url,"\n"))
 #     r <- GET(url)
 #     expect_equal(http_type(r), "text/plain")
